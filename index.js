887f0dfc82a52658d3bae45ec43e979164653b81
PoojaRajput07 <153544290+PoojaRajput07@users.noreply.github.com>
2025-03-07 16:54:25 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,81 @@
+var buttoncolors=["green","red","yellow","blue"];
+var gamepattern=[];
+var userclickedpattern=[];
+var level=0;
+function checkanswer(currentlevel){
+    if(gamepattern[currentlevel]===userclickedpattern[currentlevel]){
+        if(gamepattern.length===userclickedpattern.length){
+            setTimeout(function(){
+                nextsequence();
+            },1000)
+        }
+    }
+    else{
+        playsound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200)
+        $("h1").text("Game-Over,press any key to restart")
+        startover();
+    }
+
+}
+function startover(){
+    level=0;
+    started=false;
+    gamepattern=[];
+
+}
+function nextsequence(){
+    userclickedpattern=[];
+    level++;
+    $("h1").text("level "+ level);
+    
+
+    var n= Math.random();
+    n=n*4;
+    n=Math.floor(n);
+    var randomchosencolor=buttoncolors[n];
+    gamepattern.push(randomchosencolor);
+    $("#"+randomchosencolor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+    playsound(randomchosencolor);
+}
+    
+function playsound(name){
+    var audio=new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
+function animatepress(currentcolor){
+    $("#"+currentcolor).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentcolor).removeClass("pressed");
+
+    },100)
+
+}
+        
+
+$(".btn").click(function(){
+    var userchosencolor =$(this).attr("id")
+    userclickedpattern.push(userchosencolor);
+    checkanswer(userclickedpattern.length-1);
+    playsound(userchosencolor);
+    animatepress(userchosencolor);
+
+ })
+
+ var started=false;
+ $("h1").text("press a key to start");
+ $(document).on("keypress",function(){
+    if(!started){
+        started=true;
+        level=0;
+    nextsequence();
+    }
+ })
+
+ 
